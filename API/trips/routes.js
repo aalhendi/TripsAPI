c99322e76756a646dfd89b6f63377c84a0ecb69c
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-07-29 18:56:48 +0300

    Delete Trip Route

--- API/trips/routes.js
+++ API/trips/routes.js
@@ -1,9 +1,10 @@
 /* Imports */
 const express = require("express");
 const multer = require("multer");
-const { fetchTrips } = require("./controllers");
+const { fetchTrips, deleteTrip, fetchTrip } = require("./controllers");
 
 const router = express.Router();
+
 /* Multer */
 const storage = multer.diskStorage({
   destination: "./media",
@@ -12,10 +13,25 @@ const storage = multer.diskStorage({
   },
 });
 
+/* Param middleware */
+router.param("tripId", async (req, res, next, tripId) => {
+  const trip = await fetchTrip(tripId, next);
+  if (trip) {
+    req.trip = trip;
+    next();
+  } else {
+    const error = new Error("Trip Not Found.");
+    error.status = 404;
+    next(error);
+  }
+});
+
+/* Multer */
 const upload = multer({ storage });
 
 // Get Trips
-router.get("/trips", fetchTrips);
+router.get("/", fetchTrips);
 // router.post("/trips", upload.single("image"),createTrip);
+router.delete("/:tripId", deleteTrip);
 
 module.exports = router;
