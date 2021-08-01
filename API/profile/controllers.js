2f806ab0992c9bea802651d326d0f1e10419da93
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-08-01 17:50:25 +0300

    Add updateProfile route

--- API/profile/controllers.js
+++ API/profile/controllers.js
@@ -1,2 +1,24 @@
 /* Models */
 const { Profile } = require("../../db/models");
+// TODO: Use normalize-path
+
+exports.fetchProfile = async (profileId, next) => {
+  try {
+    const profile = await Profile.findByPk(profileId);
+    return profile;
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateProfile = async (req, res, next) => {
+  try {
+    if (req.file) {
+      req.body.image = `http://${req.get("host")}/${req.file.path}`;
+    }
+    await req.profile.update(req.body);
+    res.json(req.profile);
+  } catch (error) {
+    next(error);
+  }
+};
--- API/profile/routes.js
+++ API/profile/routes.js
@@ -1,8 +1,22 @@
 /* Imports */
 const express = require("express");
-// const { updateProfile } = require("./controllers");
+const { fetchProfile, updateProfile } = require("./controllers");
 const router = express.Router();
 
+/* Param middleware */
+router.param("profileId", async (req, res, next, profileId) => {
+  const profile = await fetchProfile(profileId, next);
+  if (profile) {
+    req.profile = profile;
+    next();
+  } else {
+    const error = new Error("Profile Not Found.");
+    error.status = 404;
+    next(error);
+  }
+});
+
 /* Update Profile */
+router.put("/:profileId", updateProfile);
 
 module.exports = router;
