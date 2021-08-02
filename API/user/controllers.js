4af691a3abcf83a376204f82a22b1be60e281ceb
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-08-02 16:14:40 +0300

    Update controllers.js
    Remove existing profile check. Not needed

--- API/user/controllers.js
+++ API/user/controllers.js
@@ -20,20 +20,6 @@ exports.register = async (req, res, next) => {
     const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
 
     /* Create Profile */
-    //👇🏻 what?🤯 how would the user who just signed up has a profile?
-    /* Check if user has a profile (sanity) */
-    const foundProfile = await Profile.findOne({
-      where: { userId: newUser.id },
-    });
-    /* Prevent user from having two profiles */
-    if (foundProfile) {
-      const error = new Error("User already has a Profile.");
-      error.status = 400;
-      return next(error);
-    }
-    // 👆🏻
-
-    /* Create empty profile */
     await Profile.create({
       userId: newUser.id,
       bio: "",
